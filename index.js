a4ca08d711165b5ab5da165e116355d544b8fec6
Nyannazz <kantemir.imam@gmail.com>
2020-04-30 17:03:03 +0200

    push before refactor working boids

--- index.js
+++ index.js
@@ -6,14 +6,14 @@ let geometry, material, mesh;
 class Boids {
     actors;
     constructor() {
-        this.camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 10);
-        this.camera.position.z = 2;
+        this.camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 120);
+        this.camera.position.z = 10;
         this.scene = new THREE.Scene();
         this.renderer = new THREE.WebGLRenderer({
             antialias: true
         });
         this.renderContainer = document.getElementById('renderContainer');
-        this.actors=[]
+        this.actors = []
     }
 
     init = () => {
@@ -41,31 +41,31 @@ class Boids {
         this.actors.push(mesh);
     }
 
-    createRandom = (count = 20) => {
+    createRandom = (count = 60) => {
         this.init();
         for (let i = 0; i < count; i++) {
             this.addActor(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5);
         }
     }
 
-    getCenter=(actors)=>{
-        const sceneSize=actors.length;
-        let averagePos=new THREE.Vector3(0, 0, 0);
-        if(!sceneSize) return averagePos;
-        
-        for(let i=0; i<sceneSize; i++){ 
+    getCenter = (actors) => {
+        const sceneSize = actors.length;
+        let averagePos = new THREE.Vector3(0, 0, 0);
+        if (!sceneSize) return averagePos;
+
+        for (let i = 0; i < sceneSize; i++) {
             averagePos.add(actors[i].position);
         }
         return averagePos.divideScalar(sceneSize);
     }
 
-    start=()=>{
+    start = () => {
         console.log(this.scene)
-        if(this.scene.children.length){
+        if (this.scene.children.length) {
             this.cameraLookAtCenter();
             this.run();
         }
-        
+
     }
 
     run = () => {
@@ -81,8 +81,8 @@ class Boids {
         }
     }
 
-    cameraLookAtCenter=()=>{
-        const center=this.getCenter(this.actors);
+    cameraLookAtCenter = () => {
+        const center = this.getCenter(this.actors);
         this.camera.lookAt(center);
     }
 
@@ -93,81 +93,101 @@ class Boids {
 }
 
 class FlyingActor extends THREE.Mesh {
-    acceleration=new THREE.Vector3(0);
-    velocity=new THREE.Vector3(0,0,0);
-    maxForce=0.001;
-    maxSpeed=0.01;
+    acceleration = new THREE.Vector3(0);
+    velocity = new THREE.Vector3(0, 0, 0);
+    maxForce = 0.008;
+    maxSpeed = 0.06;
+
+    seperationDist = 0.28;
+    allignDist = 0.6;
+
+    seperationWeight = 1.4;
+    allignmentWeight = 1.0;
+    cohesionWeight = 1.0;
+
     constructor(geometry, material) {
         super(geometry, material);
-        
+
     }
-    update=(actors)=>{
+    update = (actors) => {
         this.boidBehavior(actors)
         this.applyForce();
         this.updatePos();
+        this.acceleration.set(0, 0, 0); // reset forces
     }
 
-    boidBehavior=(actors)=>{
-        const sep=this.avoidCollision(actors);
-        
-        sep.multiplyScalar(0.2);
-        this.velocity.add(sep);
-    }
-
-    applyForce=()=>{
+    applyForce = () => {
         this.velocity.add(this.acceleration).clampLength(0, this.maxSpeed);
-        this.acceleration.set(0,0,0);
     }
 
     updatePos = () => {
         this.position.add(this.velocity);
     }
 
-    steerTo=(target)=>{
-        const targetVec=new THREE.Vector3().subVectors(this.position, target);
+    boidBehavior = (actors) => {
+        const seperation = this.seperation(actors);
+        const allign = this.allignment(actors);
+        const cohesion = this.cohesion(actors);
 
-        targetVec.setLength(this.maxSpeed);   
+        seperation.multiplyScalar(this.seperationWeight);
+        allign.multiplyScalar(this.allignmentWeight);
+        cohesion.multiplyScalar(this.cohesionWeight);
         
-        const steer=new THREE.Vector3().subVectors(this.velocity, targetVec);
+
+        this.velocity.add(seperation);
+        this.velocity.add(allign);
+        this.velocity.add(cohesion);
+        
+        if(this.position.length()>8.5){
+            
+            const homeForce=this.steerTo(new THREE.Vector3(0,0,0)).multiplyScalar(1.0);
+            this.velocity.sub(homeForce);
+        }
+        
+    }
+
+    steerTo = (target) => {
+        const targetVec = new THREE.Vector3().subVectors(target, this.position);
+
+        targetVec.setLength(this.maxSpeed);
+
+        const steer = new THREE.Vector3().subVectors(this.velocity, targetVec);
         steer.clampLength(0, this.maxForce)
         return steer;
     }
 
-    dontLeaveWorld=(distance, treshhold)=>{
+    dontLeaveWorld = (distance, treshhold) => {
         // apply force that prevents body from leaving a certain region most likely a sphere for now so distance from center should be fine
-        const vecLen=this.position.length();
-        if(vecLen>(distance-treshhold)){
-            /* console.log(vecLen) */
-            this.velocity.copy(this.velocity.negate());
+        const vecLen = this.position.length();
+        if (vecLen > (distance - treshhold)) {
+            return this.steerTo(new THREE.Vector3(0,0,0));
         }
     }
 
-    avoidCollision=(otherActors)=>{
+    seperation = (otherActors) => {
         // apply force that steers body away from others
-        const seperation=0.46;
+        const steer = new THREE.Vector3(0, 0, 0);
+        let closeActors = 0;
 
-        const steer=new THREE.Vector3(0,0,0);
-        let closeActors=0;
+        const actorsLen = otherActors.length;
 
-        const actorsLen=otherActors.length;
-        
-        for(let i=0; i<actorsLen; i++){
-            const actorDist=this.position.distanceTo(otherActors[i].position);
+        for (let i = 0; i < actorsLen; i++) {
+            const actorDist = this.position.distanceTo(otherActors[i].position);
 
-            if(actorDist>0 && actorDist<seperation){
-                const vecDir=new THREE.Vector3().subVectors(this.position, otherActors[i].position);
+            if (actorDist > 0 && actorDist < this.seperationDist) {
+                const vecDir = new THREE.Vector3().subVectors(this.position, otherActors[i].position/* , this.position */);
                 vecDir.normalize()
                 vecDir.divideScalar(actorDist);
                 steer.add(vecDir);
                 closeActors++;
             }
         }
-        if(closeActors>0){
+        if (closeActors > 0) {
             steer.divideScalar(closeActors)
 
         }
 
-        if(steer.length()>0){
+        if (steer.length() > 0) {
             steer.setLength(this.maxSpeed);
             steer.sub(this.velocity);
             steer.clampLength(0, this.maxForce)
@@ -175,16 +195,54 @@ class FlyingActor extends THREE.Mesh {
         return steer;
     }
 
-    allign=(otherActors)=>{
-        const dist=0.2;
-        const sum=new THREE.Vector3(0,0,0);
+    allignment = (otherActors) => {
+        const sum = new THREE.Vector3(0, 0, 0);
+
+        let closeActors = 0;
+        const actorsLen = otherActors.length;
+
+        for (let i = 0; i < actorsLen; i++) {
+            const actorDist = this.position.distanceTo(otherActors[i].position);
+            if (actorDist > 0 && actorDist < this.allignDist) {
+                sum.add(otherActors[i].velocity)
+                closeActors++;
+            }
+        }
+        if (closeActors > 0) {
+            sum.divideScalar(closeActors);
+            sum.setLength(this.maxSpeed);            
+            sum.sub(this.velocity);
+            sum.clampLength(0, this.maxForce);
+            return sum;
+        } else return new THREE.Vector3(0, 0, 0);
+
+    }
+
+    cohesion = (otherActors) => {
+        const sum = new THREE.Vector3(0, 0, 0);
+        let closeActors = 0;
+        const actorsLen = otherActors.length;
+
+        for (let i = 0; i < actorsLen; i++) {
+            const actorDist = this.position.distanceTo(otherActors[i].position);
+            if (actorDist > 0 && actorDist < this.allignDist) {
+                sum.add(otherActors[i].position)
+                closeActors++;
+            }
+        }
+        if (closeActors > 0) {
+            sum.divideScalar(closeActors);
+            /* console.log(sum) */
+            return this.steerTo(sum);
+        }
+        return new THREE.Vector3(0, 0, 0);
     }
 
 
-    
 
 
-    
+
+
 }
 
 function init() {
@@ -196,6 +254,3 @@ function init() {
 
 
 init();
-
-
-
