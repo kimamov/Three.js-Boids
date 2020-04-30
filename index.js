e560811368c6219fcdd86456db2014846ba5e927
Nyannazz <kantemir.imam@gmail.com>
2020-04-30 20:30:14 +0200

    some refactoring mainly split rendering and boid logic

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 import * as THREE from './node_modules/three/build/three.module.js';
 
-let camera, scene, renderer;
-let geometry, material, mesh;
 
-class Boids {
-    actors;
+
+class BoidsRenderer {
+    updateFunction;
     constructor() {
-        this.camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 120);
-        this.camera.position.z = 10;
+        this.camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 0.01, 300);
+        this.camera.position.z = 20;
         this.scene = new THREE.Scene();
         this.renderer = new THREE.WebGLRenderer({
             antialias: true
@@ -16,13 +15,14 @@ class Boids {
         this.resize();
         this.renderContainer.appendChild(this.renderer.domElement)
         window.addEventListener("resize", this.resize)
-        this.actors = []
     }
 
-    init = () => {
-        
-
-    }
+    /* init = () => {
+        const boids = new Boids();
+        boids.createRandom(30);
+        console.log(boids)
+        this.scene.add(boids.boids);
+    } */
 
     resize = () => {
         this.camera.aspect = this.renderContainer.clientWidth / this.renderContainer.clientHeight;
@@ -31,15 +31,35 @@ class Boids {
 
     }
 
-    addActor = (posX = 0, posY = 0, posZ = 0) => {
-        const geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2);
-        const material = new THREE.MeshNormalMaterial();
-        const mesh = new Boid(geometry, material);
-        mesh.velocity
-        mesh.position.set(posX, posY, posZ);
-        /* mesh.velocity.set(Math.random()/100,Math.random()/100,Math.random()/100); */
-        this.scene.add(mesh);
-        this.actors.push(mesh);
+
+    start = () => {
+        console.log(this.scene)
+        if (this.scene.children.length) {
+            if (this.updateFunction) return this.runWithUpdate()
+            this.run();
+        }
+
+    }
+
+    run = () => {
+        requestAnimationFrame(this.run);
+        this.render();
+    }
+
+    runWithUpdate = () => {
+        requestAnimationFrame(this.runWithUpdate);
+        this.updateFunction();
+        this.render();
+    }
+
+    render = () => {
+        this.renderer.render(this.scene, this.camera);
+    }
+}
+
+class Boids {
+    constructor() {
+        this.boids = new THREE.Group();
     }
 
     createRandom = (count = 60) => {
@@ -49,74 +69,98 @@ class Boids {
         }
     }
 
-    getCenter = (actors) => {
-        const sceneSize = actors.length;
+    addBoid = (boid) => {
+        this.boids.add(boid);
+        return this.boids;
+    }
+
+
+    createBoid = (posX = 0, posY = 0, posZ = 0) => {
+        const geometry = new THREE.ConeGeometry(1, 3, 5);
+        const material = new THREE.MeshNormalMaterial();
+        const mesh = new Boid(geometry, material);
+        mesh.position.set(posX, posY, posZ);
+        this.boids.add(mesh);
+        return this.boids;
+    }
+
+    createRandom = (count = 60) => {
+        for (let i = 0; i < count; i++) {
+            this.createBoid(Math.random() - 0.5, Math.random() - 0.5, Math.random() - 0.5);
+        }
+        return this.boids;
+    }
+
+    getCenter = () => {
+        const sceneSize = this.boids.children.length;
         let averagePos = new THREE.Vector3(0, 0, 0);
         if (!sceneSize) return averagePos;
 
         for (let i = 0; i < sceneSize; i++) {
-            averagePos.add(actors[i].position);
+            averagePos.add(this.boids.children[i].position);
         }
         return averagePos.divideScalar(sceneSize);
     }
 
-    start = () => {
-        console.log(this.scene)
-        if (this.scene.children.length) {
-            this.cameraLookAtCenter();
-            this.run();
-        }
-
-    }
-
-    run = () => {
-        requestAnimationFrame(this.run);
-        this.update(this.actors);
-        this.cameraLookAtCenter();
-        this.render();
-    }
-
-    update = (actors) => {
-        for (const item of this.actors) {
-            item.update(actors);
+    update = () => {
+        const boidsLen = this.boids.children.length;
+        for (let i = 0; i < boidsLen; i++) {
+            this.boids.children[i].update(this.boids.children);
         }
     }
 
-    cameraLookAtCenter = () => {
-        const center = this.getCenter(this.actors);
-        this.camera.lookAt(center);
-    }
 
-    render = () => {
-        this.renderer.render(this.scene, this.camera);
-    }
+
 
 }
 
+/* class Boids {
+
+} */
+
 class Boid extends THREE.Mesh {
-    acceleration = new THREE.Vector3(0);
-    velocity = new THREE.Vector3(0, 0, 0);
-    
-    maxForce = 0.006;
-    maxSpeed = 0.08;
 
-    seperationDist = 0.08;
-    allignDist = 0.6;
-    homeDist=20.0;
+    constructor(
+        geometry = new THREE.ConeGeometry(1, 3, 5),
+        material = new THREE.MeshNormalMaterial(),
+        options = {
+            accelerationVector: new THREE.Vector3(),
+            velocityVector: new THREE.Vector3(),
+            maxForce: 0.03,
+            maxSpeed: 0.4,
+            seperationDist: 1.1,
+            allignDist: 10,
+            homeDist: 200.0,
+            seperationWeight: 1.5,
+            allignmentWeight: 1.1,
+            cohesionWeight: 1.0
+        }
+    ) {
+        super(geometry, material); // create the actual mesh
 
-    seperationWeight = 1.5;
-    allignmentWeight = 1.1;
-    cohesionWeight = 1.0;
+        this.acceleration = options.accelerationVector;
+        this.velocity = options.velocityVector;
 
-    constructor(geometry, material) {
-        super(geometry, material);
+        this.maxForce = options.maxForce;
+        this.maxSpeed = options.maxSpeed;
 
+        this.seperationDist = options.seperationDist;
+        this.allignDist = options.allignDist;
+        this.homeDist = options.homeDist;
+        this.seperationWeight = options.seperationWeight;
+        this.allignmentWeight = options.allignmentWeight;
+        this.cohesionWeight = options.cohesionWeight;
     }
     update = (actors) => {
         this.boidBehavior(actors)
         this.applyForce();
+        this.rotateMesh();
         this.updatePos();
         this.acceleration.set(0, 0, 0); // reset forces
+    }
+
+    rotateMesh=()=>{
+        this.quaternion.setFromUnitVectors(new THREE.Vector3(0,1,0), this.velocity.clone().normalize());
     }
 
     applyForce = () => {
@@ -135,17 +179,17 @@ class Boid extends THREE.Mesh {
         seperation.multiplyScalar(this.seperationWeight);
         allign.multiplyScalar(this.allignmentWeight);
         cohesion.multiplyScalar(this.cohesionWeight);
-        
+
 
         this.acceleration.add(seperation);
         this.acceleration.add(allign);
         this.acceleration.add(cohesion);
-        
-        if(this.position.length()>this.homeDist){
-            const homeForce=this.steerTo(new THREE.Vector3(0,0,0)).multiplyScalar(1.0);
+
+        if (this.position.length() > this.homeDist) {
+            const homeForce = this.steerTo(new THREE.Vector3(0, 0, 0)).multiplyScalar(1.0);
             this.acceleration.sub(homeForce);
         }
-        
+
     }
 
     steerTo = (target) => {
@@ -162,7 +206,7 @@ class Boid extends THREE.Mesh {
         // apply force that prevents body from leaving a certain region most likely a sphere for now so distance from center should be fine
         const vecLen = this.position.length();
         if (vecLen > (distance - treshhold)) {
-            return this.steerTo(new THREE.Vector3(0,0,0));
+            return this.steerTo(new THREE.Vector3(0, 0, 0));
         }
     }
 
@@ -212,7 +256,7 @@ class Boid extends THREE.Mesh {
         }
         if (closeActors > 0) {
             sum.divideScalar(closeActors);
-            sum.setLength(this.maxSpeed);            
+            sum.setLength(this.maxSpeed);
             sum.sub(this.velocity);
             sum.clampLength(0, this.maxForce);
             return sum;
@@ -248,10 +292,16 @@ class Boid extends THREE.Mesh {
 }
 
 function init() {
+    const boidsRenderer = new BoidsRenderer();
     const boids = new Boids();
-    /* boids.init(); */
-    boids.createRandom();
-    boids.start();
+    boids.createRandom(600);
+    boidsRenderer.scene.add(boids.boids);
+    boidsRenderer.updateFunction = () => {
+        boids.update();
+        boidsRenderer.camera.lookAt(boids.getCenter())
+    };
+    /* boids.createRandom(); */
+    boidsRenderer.start();
 }
 
 
